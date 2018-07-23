0ed43b6ff426dd153d58b55c8ba2df3d226fe65c
Josh <Gaineyj0349@gmail.com>
2018-07-22 20:08:16 -0400

    updated

--- app/src/main/java/com/gainwise/seed/MainActivity.kt
+++ app/src/main/java/com/gainwise/seed/MainActivity.kt
@@ -2,16 +2,11 @@ package com.gainwise.seed
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.gainwise.seed.ExtensionFunctions.getLocaleDateString
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        iv.minZoom = .5f
-        iv.maxZoom = 1000f
-        tv.text = System.currentTimeMillis().getLocaleDateString()
     }
 }
--- seed/src/main/java/com/gainwise/seed/Vitals/CrashAllocator.kt
+++ seed/src/main/java/com/gainwise/seed/Vitals/CrashAllocator.kt
@@ -1,12 +1,19 @@
 package com.gainwise.seed.Vitals
 
+import android.content.Context
+import com.gainwise.seed.ExtensionFunctions.causeAndStackTraceToString
+
 class CrashAllocator(//declare variable for object imlementing interface
-        private val ourCrashHandler: Crashable) {
+        private val ourCrashHandler: Crashable, context: Context, prefsNameToSaveStackTrace: String) {
 
     //declare variable for uncaught exception handler
     private var defaultCrashHandler: Thread.UncaughtExceptionHandler? = null
 
     private val TUEhandler = Thread.UncaughtExceptionHandler { thread, ex ->
+
+        val prefs = context.getSharedPreferences(prefsNameToSaveStackTrace, Context.MODE_PRIVATE).edit();
+        prefs.putString("CRASH", ex.causeAndStackTraceToString())
+        prefs.apply()
         //Code passed via the Crashable interface in the constructor is executed
         ourCrashHandler.executeOnCrash()
 
